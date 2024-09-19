823f11f3836df5a68aa9c189a39e167ece7e57ba
Mhycka1 <mmcdon10@terpmail.umd.edu>
2024-09-19 11:58:54 -0400

    added 2-opt algo that doesn't work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import numpy as np
-from part1 import make_graph, nearest_neighbor
+from part1 import make_graph, nearest_neighbor, nearest_neighbor_2opt
 
 
 def main():
@@ -9,30 +9,34 @@ def main():
     size = int(size)
     matrix = []
     
-    # # Read the actual matrix
+    # Read the actual matrix
     for i in range(size):
         line = sys.stdin.readline().strip()
         matrix.append([int(x) for x in line.split()])  
     adjacency_matrix = np.array(matrix)
+
     path, cost = nearest_neighbor(adjacency_matrix, 0)
     print("Visited path:", path)
     print("Total cost:", cost)
 
-    #part 1 of the assignment
-    size_5_graphs = [] 
-    size_10_graphs = []
-    size_15_graphs = []
-    size_20_graphs = []
-    size_25_graphs = []
-    size_30_graphs = []
+    best = nearest_neighbor_2opt(adjacency_matrix)
+    print("NN20 solution:", best)
 
-    for i in range(30):
-        size_5_graphs.append(make_graph(5))
-        size_10_graphs.append(make_graph(10))
-        size_15_graphs.append(make_graph(15))
-        size_20_graphs.append(make_graph(20))
-        size_25_graphs.append(make_graph(25))
-        size_30_graphs.append(make_graph(30))
+    #part 1 of the assignment
+    # size_5_graphs = [] 
+    # size_10_graphs = []
+    # size_15_graphs = []
+    # size_20_graphs = []
+    # size_25_graphs = []
+    # size_30_graphs = []
+
+    # for i in range(30):
+    #     size_5_graphs.append(make_graph(5))
+    #     size_10_graphs.append(make_graph(10))
+    #     size_15_graphs.append(make_graph(15))
+    #     size_20_graphs.append(make_graph(20))
+    #     size_25_graphs.append(make_graph(25))
+    #     size_30_graphs.append(make_graph(30))
         
 
 if __name__ == '__main__':
--- part1.py
+++ part1.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
 import networkx as nx
+from py2opt.routefinder import RouteFinder
 
 
 # #method to generate graphs
@@ -30,12 +31,36 @@ def nearest_neighbor(adj_matrix, start):
 
     for i in range(N-1):
         last = path[-1]
-        next_ind = np.argmin(adj_matrix[last][mask]) # find minimum of remaining locations
-        next_loc = np.arange(N)[mask][next_ind] # convert to original location
-        path.append(next_loc)
+        next_ind = np.argmin(adj_matrix[last][mask]) 
+        next_loc = np.arange(N)[mask][next_ind] 
+        path.append(int(next_loc))
         mask[next_loc] = False
         cost += adj_matrix[last, next_loc]
 
     return path, cost
 
+def nearest_neighbor_2opt(adj_matrix):
+    path, cost = nearest_neighbor(adj_matrix, 0)
+    optimized_route = two_opt(path, adj_matrix)
 
+
+# edited from the below stackoverflow post
+# https://stackoverflow.com/questions/53275314/2-opt-algorithm-to-solve-the-travelling-salesman-problem-in-python
+def cost_change(cost_mat, n1, n2, n3, n4):
+    return cost_mat[n1][n3] + cost_mat[n2][n4] - cost_mat[n1][n2] - cost_mat[n3][n4]
+
+# edited from the same stackoverflow post as above
+# https://stackoverflow.com/questions/53275314/2-opt-algorithm-to-solve-the-travelling-salesman-problem-in-python
+def two_opt(route, cost_mat):
+    best = route
+    improved = True
+    while improved:
+        improved = False
+        for i in range(1, len(route) - 2):
+            for j in range(i + 1, len(route)):
+                if j - i == 1: continue
+                if cost_change(cost_mat, best[i - 1], best[i], best[j - 1], best[j]) < 0:
+                    best[i:j] = best[j - 1:i - 1:-1]
+                    improved = True
+        route = best
+    return best
