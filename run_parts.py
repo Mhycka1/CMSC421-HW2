cfd39a7d2dc32f0b6c21bbb970537aff4705eccc
Mhycka1 <mmcdon10@terpmail.umd.edu>
2024-09-24 20:10:30 -0400

    added part3 orint statements

--- run_parts.py
+++ run_parts.py
@@ -1,6 +1,7 @@
 # here is where all the parts will be run
 from part1 import make_graph, process_graph_family, make_part1_graphs, read_stats
 from part2 import run_A_star, run_nn, run_nn2o, run_rnn, compute_differences, make_part2_graphs
+from part3 import run_hill_climbing, run_simuAnnealing, run_genetic
 import os
 import matplotlib.pyplot as plt
 import numpy as np
@@ -128,13 +129,61 @@ def main():
     a_5 = run_A_star(size_5_graphs)
     a_6 = run_A_star(size_6_graphs)
     a_7 = run_A_star(size_7_graphs)
-    a_8 = run_A_star(size_8_graphs)
-    a_9 = run_A_star(size_9_graphs)
-    a_10 = run_A_star(size_10_graphs)
-        
-    
 
-    
+    #hill climbing cluster
+    h_5_1 = run_hill_climbing(size_5_graphs)
+    h_6_1 = run_hill_climbing(size_6_graphs)
+    h_7_1 = run_hill_climbing(size_7_graphs)
+
+    h_5_2 = run_hill_climbing(size_5_graphs, 2)
+    h_6_2 = run_hill_climbing(size_6_graphs, 2)
+    h_7_2 = run_hill_climbing(size_7_graphs, 2)
+
+    h_5_3 = run_hill_climbing(size_5_graphs, 3)
+    h_6_3 = run_hill_climbing(size_6_graphs, 3)
+    h_7_3 = run_hill_climbing(size_7_graphs, 3)
+
+    print("Hill Climbing Results:")
+    print("Size 5, Restarts 1:", h_5_1)
+    print("Size 6, Restarts 1:", h_6_1)
+    print("Size 7, Restarts 1:", h_7_1)
+    print("Size 5, Restarts 2:", h_5_2)
+    print("Size 6, Restarts 2:", h_6_2)
+    print("Size 7, Restarts 2:", h_7_2)
+    print("Size 5, Restarts 3:", h_5_3)
+    print("Size 6, Restarts 3:", h_6_3)
+    print("Size 7, Restarts 3:", h_7_3)
+
+    #simulated annealing cluster
+    s_5_1_1000_95 = run_simuAnnealing(size_5_graphs)
+    s_6_1_1000_95 = run_simuAnnealing(size_6_graphs)
+    s_7_1_1000_95 = run_simuAnnealing(size_7_graphs)
+
+    s_5_2_1000_95 = run_simuAnnealing(size_5_graphs, restarts=2)
+    s_6_2_1000_95 = run_simuAnnealing(size_6_graphs, restarts=2)
+    s_7_2_1000_95 = run_simuAnnealing(size_7_graphs, restarts=2)
+
+    s_5_1_2000_95 = run_simuAnnealing(size_5_graphs, initial_temp=2000)
+    s_6_1_2000_95 = run_simuAnnealing(size_6_graphs, initial_temp=2000)
+    s_7_1_2000_95 = run_simuAnnealing(size_7_graphs, initial_temp=2000)
+
+    s_5_1_1000_85 = run_simuAnnealing(size_5_graphs, alpha=0.85)
+    s_6_1_1000_85 = run_simuAnnealing(size_6_graphs, alpha=0.85)
+    s_7_1_1000_85 = run_simuAnnealing(size_7_graphs, alpha=0.85)
+
+    print("\nSimulated Annealing Results:")
+    print("Size 5, Initial Temp 1000, Alpha 95:", s_5_1_1000_95)
+    print("Size 6, Initial Temp 1000, Alpha 95:", s_6_1_1000_95)
+    print("Size 7, Initial Temp 1000, Alpha 95:", s_7_1_1000_95)
+    print("Size 5, Restarts 2, Alpha 95:", s_5_2_1000_95)
+    print("Size 6, Restarts 2, Alpha 95:", s_6_2_1000_95)
+    print("Size 7, Restarts 2, Alpha 95:", s_7_2_1000_95)
+    print("Size 5, Initial Temp 2000, Alpha 95:", s_5_1_2000_95)
+    print("Size 6, Initial Temp 2000, Alpha 95:", s_6_1_2000_95)
+    print("Size 7, Initial Temp 2000, Alpha 95:", s_7_1_2000_95)
+    print("Size 5, Initial Temp 1000, Alpha 85:", s_5_1_1000_85)
+    print("Size 6, Initial Temp 1000, Alpha 85:", s_6_1_1000_85)
+    print("Size 7, Initial Temp 1000, Alpha 85:", s_7_1_1000_85)
 
 if __name__ == "__main__":
     main()
